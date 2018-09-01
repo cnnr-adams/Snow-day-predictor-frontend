a75f8991cfb6875e01422e3138801f11b76d1c44
Caelan Douglas <caelan.douglas@outlook.com>
2018-09-01 19:04:56 -0400

    Input fields that redirect to new page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,24 @@
 console.log("yeet on the haters");
+
+let postalCode = "";
+let date = "";
+
+function inputPostalCode(value) {
+    postalCode = value;
+    console.log(`yeet on the ${value}`);
+}
+
+function inputDate(value) {
+    date = value;
+    console.log(`yeet on the ${value}`);
+}
+
+function submit() {
+    if (postalCode.length > 0 && date.length > 0) {
+        console.log("done yeeted");
+        window.location.href = `result/${postalCode}/${date}`;
+    } else {
+        console.log("go commit die");
+    }
+}
+
