68279e770683522a2bc989b4a9819125ee63f088
cnnr-adams <cnnr252@gmail.com>
2018-09-01 17:55:41 -0400

    Set up web server with entry points for html, css, js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+/* serves main page */
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/user/add", function (req, res) {
+    /* some server side logic */
+    res.send("OK");
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function (req, res) {
+    console.log('static file request : ' + req.params);
+    res.sendfile(__dirname + req.params[0]);
+});
+
+
+var port = process.env.PORT || 25565;
+app.listen(port, function () {
+    console.log("Listening on " + port);
+});
