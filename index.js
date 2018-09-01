f0d0a2eb85c0232a66033f5a20d8b6b3609b1154
cnnr-adams <cnnr252@gmail.com>
2018-09-01 18:43:01 -0400

    Began results page

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post("/user/add", function (req, res) {
-    /* some server side logic */
-    res.send("OK");
-});
+app.get("/result/:postalCode/:date", function (req, res) {
+    console.log(req.params);
+    res.sendFile(__dirname + '/result/index.html');
+})
 
 /* serves all the static files */
 app.get(/^(.+)$/, function (req, res) {
-    console.log('static file request : ' + req.params);
-    res.sendfile(__dirname + req.params[0]);
-});
-
+    res.sendFile(__dirname + req.params[0]);
+})
 
 var port = process.env.PORT || 25565;
 app.listen(port, function () {
